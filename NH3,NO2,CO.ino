a16097a598069293121d36d26e7a94d88f46d777
kangsm0903 <kangsm0903@naver.com>
2022-06-01 11:24:17 +0900

    NH3, NO2, CO edit

--- NH3,NO2,CO.ino
+++ NH3,NO2,CO.ino
@@ -1,3 +1,32 @@
+const int S_analog = 1023.0;
+
+int CO, NH3;
+float NO2;
+
+void setup(){
+
+  Serial.begin(9600);
+  Serial.println("MiCS 6814 simple test");
+}
+
+void loop(){
+  CO = map(analogRead(A3), 0, S_analog, 1, 1000);
+  NH3 = map(analogRead(A1), 0, S_analog, 1, 500);
+  NO2 = (map(analogRead(A2), 0, S_analog, 1, 1000))/100.0;
+
+  Serial.print("NH3: ");
+  Serial.println(NH3);
+  Serial.print("NO2: ");
+  Serial.println(NO2);
+  Serial.print("CO: ");
+  Serial.println(CO);        
+  Serial.println();
+  
+  delay(5000);
+}
+
+// ------------------------------------------------------------------------
+
 // MiCS 6814 simple test
 // NH3: 236, NO2: 294, CO: 290
 
